5b459bc6ca0b6d4ef3986f767e5239cdabe3dbd5
Kristjan Onu <kristjan.onu@canada.ca>
2019-01-23 22:04:44 +0000

    Add LGPL license

--- src/burp_api.c
+++ src/burp_api.c
@@ -17,6 +17,27 @@
  *               CALLING BURP SUBROUTINES DIRECTLY.
  *               NOT ALL ARE COVERED, ONLY THOSE THAT ARE MOSTLY USED ARE WRAPPED
  *
+ *  Copyright (C) 2019  Government of Canada
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301 USA
+ *
+ *  The Environment and Climate Change Canada open source licensing
+ *  due diligence process form for libburpc was approved on December
+ *  21, 2018.
+ *
  */
 #include <stdio.h>
 #include <sys/types.h>
